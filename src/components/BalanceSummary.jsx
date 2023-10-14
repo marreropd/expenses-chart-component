2f20d97ff51518774559b3ac53b806fad6fa2bd6
Pablo Marrero <pablomarrerofernandez1@gmail.com>
2023-10-14 21:26:54 +0200

    fix images to deploy

--- src/components/BalanceSummary.jsx
+++ src/components/BalanceSummary.jsx
@@ -40,7 +40,17 @@ const BalanceSummary = () => {
         <h1>
           My balance<span>$921.48</span>
         </h1>
-        <img src="../public/images/logo.svg" alt="" />
+        <svg
+          width="72"
+          height="48"
+          viewBox="0 0 72 48"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <g fill="none" fill-rule="evenodd">
+            <circle fill="#382314" cx="48" cy="24" r="24" />
+            <circle stroke="#FFF" stroke-width="2" cx="24" cy="24" r="23" />
+          </g>
+        </svg>
       </header>
       <section className="spending-card">
         <h2> Spending - Last 7 days</h2>
